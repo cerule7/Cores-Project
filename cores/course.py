c2015171a86985068e83be2fc69b86b6f1339b7e
Noah Green <noahc.green@icloud.com>
2018-02-04 19:41:51 -0500

    Removed default values from Course

--- cores/course.py
+++ cores/course.py
@@ -1,10 +1,4 @@
 class Course:
-	name = ""
-	number = ""
-	cores = {"WC": False, "WCr": False, "WCd": False, 
-	"NS": False, "SCL": False, "HST": False, "QQ": False, 
-	"QR": False, "ITR": False,"CC": False, "Ahp": False, 
-	"Ahq": False, "Aho": False, "Ahr": False}
 
 	def __init__(self, name, number, cores):
 		self.name = name
